0e67abdc222d00a0b4dde4f5075caeab70746add
YeonDoIt <duswo5170@gmail.com>
2024-01-16 19:33:31 +0900

    2024-01-16

--- 자바페스티벌/src/day1/prac.java
+++ 자바페스티벌/src/day1/prac.java
@@ -1,13 +1,27 @@
 package day1;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class prac {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.print("행 개수 : ");
-		String num = sc.next();
 
+		int[][] array = new int[5][5];
+		int num = 1;
+		
+		for(int i = 0; i < array.length; i++) {
+			for(int j = array.length - 1; j >= 0; j--) {
+				array[j][i] = num++;
+			}
+		}
+		
+		for(int i = 0; i < array.length; i++) {
+			for(int j = 0; j <array.length; j++) {
+				System.out.print(array[i][j] + "\t");
+			}
+			System.out.println();
+		}
+		
 	}
 }
--- 자바페스티벌/src/최종/problem_20.java
+++ 자바페스티벌/src/최종/problem_20.java
@@ -10,21 +10,18 @@ public class problem_20 {
 		System.out.print("숫자 입력 >> ");
 		int num = sc.nextInt();
 		String result = "";
-<<<<<<< HEAD
 		
 		while(num > 0) {
 			result = result + (num % 2);
 			num = num / 2;
 		}
 		
-=======
 
 		while(num > 0) {
 			result = (num % 10) + " " + result;
 			num = num / 10;
 		}
 
->>>>>>> 440e92d1ad1e10e4057afe334311019689584577
 		System.out.println(result);
 	}
 
